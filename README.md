78eb1e822aeabc540bce1861a8f6bde298018d7b
lucasisoppo <isoppolucas@gmail.com>
2018-02-23 15:17:34 -0300

    sdvsdv

---
+++ README.md
@@ -0,0 +1,2 @@
+# Boilerplate Heroku
+This is just boilerplate code for deploying JavaEE apps to Heroku easily. Just push to your git repo ;)
--- target/SistemaImoveis-1.0/controller/casasJson.js
+++
@@ -1,7 +0,0 @@
-var casasModulo = angular.module('casasModulo', []);
-
-casasModulo.controller ("casasController", function($scope, $http) {
-	$http.get ('controller/casasJson.json').then(function(response){
-		$scope.casas = response.data.casas;
-	})
-});
